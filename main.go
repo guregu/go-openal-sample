613d2cfe51746c7c4aa04cebc7d819ae6a8d5320
Greg <greg@toki.waseda.jp>
2015-07-30 22:20:42 +0900

    lots of stuff

--- main.go
+++ main.go
@@ -1,101 +1,120 @@
 package main
 
 import (
-    al "azul3d.org/native/al.v1"
-    audio "azul3d.org/audio.v1"
-    _ "azul3d.org/audio/wav.v1"
-    "log"
-    "math/rand"
-    "unsafe"
-    "time"
-    "os"
+	audio "azul3d.org/audio.v1"
+	_ "azul3d.org/audio/wav.v1"
+	al "azul3d.org/native/al.v1"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+	"unsafe"
 )
 
 const (
-    freq = 44100 // 44.1 kHz
-    duration = 3 // 3 seconds
+	freq = 44100 // 44.1 kHz
 )
 
+func init() {
+	// al.SetErrorHandler(func(e error) {
+	// 	panic(e)
+	// })
+}
+
 func main() {
-    var data []int16
-    switch len(os.Args) {
-        case 1:
-        log.Println("Loading white noise.")
-        data = genWhiteNoise()
-        break
+	var data []audio.PCM16
+	var duration float64
+	var config audio.Config
+	switch len(os.Args) {
+	case 1:
+		log.Println("Loading white noise.")
+		duration = 3
+		data = genWhiteNoise(duration)
+		config = audio.Config{SampleRate: freq, Channels: 1}
+		break
 
-        case 2:
-        filename := os.Args[1]
-        log.Printf("Loading %s\n", filename)
-        data = readFile(filename)
-        break
+	case 2:
+		filename := os.Args[1]
+		log.Printf("Loading %s\n", filename)
+		data, duration, config = readFile(filename)
+		data, duration, config = readFile(filename)
+		break
 
-        default:
-        log.Fatalf("Unexpected number of arguments: %s. Must be 1 or 2.\nUsage: go run main.go [file]\n", len(os.Args))
-    }
+	default:
+		log.Panicf("Unexpected number of arguments: %d. Must be 1 or 2.\nUsage: go run main.go [file]\n", len(os.Args))
+	}
 
+	log.Println("Duration:", duration, "seconds")
 
-    device, err := al.OpenDevice("", nil)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer device.Close()
+	device, err := al.OpenDevice("", nil)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer device.Close()
 
-    var buffers uint32 = 0
-    device.GenBuffers(1, &buffers)
-    device.BufferData(buffers, al.FORMAT_MONO16, unsafe.Pointer(&data[0]), int32(int(unsafe.Sizeof(data[0]))*len(data)), freq)
-    var sources uint32 = 0
-    device.GenSources(1, &sources)
-    device.Sourcei(sources, al.BUFFER, int32(buffers))
-    device.SourcePlay(sources)
+	var buffers uint32 = 0
+	device.GenBuffers(1, &buffers)
+	if config.Channels == 1 {
+		device.BufferData(buffers, al.FORMAT_MONO16, unsafe.Pointer(&data[0]), int32(int(unsafe.Sizeof(data[0]))*len(data)), int32(config.SampleRate))
+	} else {
+		device.BufferData(buffers, al.FORMAT_STEREO16, unsafe.Pointer(&data[0]), int32(int(unsafe.Sizeof(data[0]))*len(data)), int32(config.SampleRate))
+	}
+	var sources uint32 = 0
+	device.GenSources(1, &sources)
+	device.Sourcei(sources, al.BUFFER, int32(buffers))
+	device.SourcePlay(sources)
 
-    time.Sleep(time.Second * duration)
+	time.Sleep(time.Duration(duration * float64(time.Second)))
 
-    device.DeleteSources(1, &sources)
-    device.DeleteBuffers(1, &buffers)
-    log.Println("Done.")
+	device.DeleteSources(1, &sources)
+	device.DeleteBuffers(1, &buffers)
+	log.Println("Done.")
 }
 
-func readFile(filename string) []int16 {
-    file, err := os.Open(filename)
-    if err != nil {
-        log.Fatal(err)
-    }
+func readFile(filename string) ([]audio.PCM16, float64, audio.Config) {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Panic(err)
+	}
 
-    // Create a decoder for the audio source
-    decoder, format, err := audio.NewDecoder(file)
-    if err != nil {
-        log.Fatal(err)
-    }
-    config := decoder.Config()
-    log.Printf("Decoding a %s file.\n", format)
-    log.Println(config)
+	fi, err := file.Stat()
+	if err != nil {
+		log.Panic(err)
+	}
 
-    // Create a buffer that can hold 3 second of audio samples
-    bufSize := duration * config.SampleRate * config.Channels
-    buf := make(audio.F64Samples, bufSize)
+	// Create a decoder for the audio source
+	decoder, format, err := audio.NewDecoder(file)
+	if err != nil {
+		log.Panic("err: %T %v %#v", err, err, err)
+	}
+	config := decoder.Config()
+	log.Printf("Decoding a %s file.\n", format)
+	log.Println(config)
 
-    // Fill the buffer with as many audio samples as we can
-    read, err := decoder.Read(buf)
-    if err != nil {
-        log.Fatal(err)
-    }
-    readBuf := buf.Slice(0, read)
-    data := make([]int16, readBuf.Len())
-    for i := 0; i < readBuf.Len(); i++ {
-        data[i] = int16(audio.F64ToPCM16(readBuf.At(i)))
-    }
-    return data
+	time := float64(fi.Size()) / float64(config.SampleRate*config.Channels*16/8)
+
+	// Create a buffer that can hold 3 second of audio samples
+	bufSize := int(time * float64(config.SampleRate*config.Channels))
+	// Most WAVs are PCM16
+	buf := make(audio.PCM16Samples, bufSize)
+
+	// Fill the buffer with as many audio samples as we can
+	read, err := decoder.Read(buf)
+	if err != nil && err != audio.EOS {
+		log.Panic(err)
+	}
+
+	return []audio.PCM16(buf)[:read], time, config
 }
 
-func genWhiteNoise() []int16 {
-    data := make([]int16, freq*duration)
-    for i := 0; i < freq*duration; i++ {
-        data[i] = rnd(-32767, 32767);
-    }
-    return data
+func genWhiteNoise(duration float64) []audio.PCM16 {
+	data := make([]audio.PCM16, int(float64(freq)*duration))
+	for i := 0; i < len(data); i++ {
+		data[i] = rnd(-32767, 32767)
+	}
+	return data
 }
 
-func rnd(min, max int) int16 {
-    return int16(min + (rand.Intn(max - min)))
+func rnd(min, max int) audio.PCM16 {
+	return audio.PCM16(min + (rand.Intn(max - min)))
 }
